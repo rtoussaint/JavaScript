9d2e7566e43e581b606aa00f0560b2dea61b57c8
rtoussaint <rmtoussaint@gmail.com>
2013-04-16 20:08:40 -0300

    Update q1-multiples.js

--- assignments/q1-multiples.js
+++ assignments/q1-multiples.js
@@ -1,13 +1,20 @@
-function multiples(limit) {
-  var sum = 0;
-  for (var i = 3; i < limit; i += 1) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-  }
-  return sum;
-}
+function multiples (limit) {
+	var counter = 0;
+	for(var i = 0; i < limit; i++) {
+		if(i%3 == 0){
+			counter = counter + i;
+		}
+		else if(i%5 == 0){
+			counter = counter + i;
+		}
+		else{
+			counter = counter;
+		}
+		
+	}
+	return counter;
 
+}
 console.log(multiples(1));
 console.log(multiples(10));
 console.log(multiples(16));
